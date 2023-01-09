76e13ed4d7e6001f044d784eb2473c516addb203
Ani Loprais <an.loprais@gmail.com>
2023-01-09 19:47:51 +0100

    Regex phone

---
+++ RegexPhone/RegexPhone/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text.RegularExpressions;
+public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Write the phone number: ");
+        var phonenumber = Console.ReadLine();
+        CheckMatch(phonenumber);
+    }
+
+    public static void CheckMatch(string phonenumber)
+    {
+        var reg = new Regex("^\\+(420|\\+421) ?[0-9]{3} ?[0-9]{3} ?[0-9]{3}$");
+        if (!reg.IsMatch(phonenumber))
+        {
+            Console.WriteLine($"{phonenumber} is an incorrect format");
+        }
+        else
+        {
+            Console.WriteLine($"{phonenumber} is correct format");
+        }
+        Console.ReadLine();
+    }
+}
