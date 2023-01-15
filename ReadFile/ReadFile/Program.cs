cb4730b801be87802d15fffdba83bb886b535b8f
Ani Loprais <an.loprais@gmail.com>
2023-01-15 21:45:36 +0100

    Program to open the file

---
+++ ReadFile/ReadFile/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace ReadFile
+{
+    class OpenRead
+    {
+        static void Main(string[] args)
+        {
+
+            string fileName = @"/Users/anicka/Desktop/csharp/ReadFile/ReadFile/basen.txt";
+
+            string fileRead= File.ReadAllText(fileName);
+
+            Console.WriteLine(fileRead);
+
+            //byte[] buf = new byte[1024];
+            //int c;
+
+            //while ((c = fs.Read(buf, 0, buf.Length)) > 0)
+            //{
+            //    Console.WriteLine(Encoding.UTF8.GetString(buf, 0, c));
+            //}
+
+            Console.ReadLine();
+
+        }
+
+
+    }
+}
