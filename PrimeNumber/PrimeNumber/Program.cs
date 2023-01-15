04a7ef3677db401ce036c1cb42e960232b922df1
Ani Loprais <an.loprais@gmail.com>
2023-01-15 21:59:26 +0100

    PrimeNumber check

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Primenumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number you want check.");
+            int number1 = Convert.ToInt32(Console.ReadLine());
+            int divisors = 0;
+
+            for (int i = 1; i <= number1; i++)
+            {
+                if (number1 % i == 0)
+                {
+                    divisors++;
+                }
+            }
+
+            if (divisors == 2)
+            {
+                Console.WriteLine("The entered number is a Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("The entered number is not a Prime Number");
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
